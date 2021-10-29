7e598e29f8a55bdd929e738cfc82e95d65c07038
LuizAntonioPereira <luiz.antonio.dragao@gmail.com>
2021-10-29 18:41:19 -0300

    update
    update

--- Script/Control.gd
+++ Script/Control.gd
@@ -3,7 +3,10 @@ extends Control
 const CONNECT_TEXT = ["Waiting for players...", "Connecting to server..."]
 
 func set_connect_type(host):	
-
+	get_node("../VBoxContainer").visible = false
+	get_node("../VBoxContainer2").visible = false
+	get_node("../VBoxContainer3").visible = false
+	#get_parent().get_node("Nome do Node").visible = false
 	show()
 	if host:
 		$Connecting/Label.text = CONNECT_TEXT[0]
